84cced80c097117e0be26b990dcb728bb2221c8f
Ken <gsl618@hotmail.com>
2020-04-25 19:50:35 -0400

    attachments

--- frontend/src/components/attachments/attachment.js
+++ frontend/src/components/attachments/attachment.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup'
+import Media from 'react-bootstrap/Media'
+import "./attachment.css"
 
 const Attachment = (props) => {
     return (
-      <ListGroup horizontal='sm'>
-        <ListGroup.Item>{props.attachment_type}</ListGroup.Item>
-        <ListGroup.Item>{props.attachment_name}</ListGroup.Item>
-      </ListGroup>
+      <Media>
+        <ListGroup className="col-sm-2">
+          <ListGroup.Item className="attachmentType">{props.attachment_type}</ListGroup.Item>
+          <ListGroup.Item className="attachmentName">{props.attachment_name}</ListGroup.Item>
+        </ListGroup>
+        <img
+          height={100}
+          src={require("../../imgs/placeholder.jpg")}
+        />
+      </Media>
     );
 }
 
